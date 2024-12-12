fac1023f3fc75ea196b9cae87696192203096888
Alejandro G <algiraldohe@gmail.com>
2024-12-12 16:16:47 +0000

    task(modelling): adding new kpi to the data mart.

--- orchestration/dbt/superstore/models/mart/mart_trading_disccounts.sql
+++ orchestration/dbt/superstore/models/mart/mart_trading_disccounts.sql
@@ -9,6 +9,7 @@ with base_data as (
         , {{ products_alias }}.product_name
         , {{ products_alias }}.category as product_category
         , {{ products_alias }}.sub_category as product_sub_category
+        , {{ orders_alias }}.order_id
         , case when {{ orders_alias }}.discount = 0.00 then 0 else 1 end as is_discount
         , {{ customers_alias }}.customer_id
         , {{ geo_alias }}.region
@@ -76,6 +77,24 @@ sales_disccounts as (
     
     from base_data
     {{ dbt_utils.group_by(10) }}
+),
+
+orders_disccounts as (
+    select 
+        'orders-disccounts' as kpi
+        , fiscal_year_month_desc
+        , product_name
+        , product_category
+        , product_sub_category
+        , is_discount
+        , region
+        , state
+        , city
+        , segment
+        , count(distinct order_id) as value
+    
+    from base_data
+    {{ dbt_utils.group_by(10) }}
 )
 
 select * from products_disccounts
@@ -83,3 +102,5 @@ union all
 select * from customers_disccounts
 union all
 select * from sales_disccounts
+union all
+select * from orders_disccounts
